94f2f6d841d2905a42414e283efa608a429b642d
megh21 <73994639+megh21@users.noreply.github.com>
2022-05-22 03:32:35 +0200

    Add files via upload

--- aux_code.py
+++ aux_code.py
@@ -5,22 +5,18 @@ Timeseries Classification with Transformers
 
 from dataclasses import dataclass
 import os
-from time import time
-import typing
 import numpy as np
 from sklearn.model_selection import train_test_split
 from lstm import lstm 
-import pandas as pd
 # Import packages as you need
 
-def load_data(data_path : str) -> pd.DataFrame:
+
+def load_data(data_path):
     """
     Loading of the dataset provided
     Edit the code below
     """
-    data=pd.read_pickle(data_path)
-    columns = data.columns
-    data[columns[0]] = data[columns[0]].apply(lambda x: x.reshape((500,1)))
+    data=None
     return data
 
 
@@ -29,10 +25,6 @@ def preprocess_data(data):
     A standard nan removal to be added.
     Add more preprocessing steps if needed.
     """
-    data = data.dropna()
-    columns = data.columns
-    data[columns[1]] = data[columns[1]].apply(lambda x: int(x))
-    data[columns[1]] = [0 for x in data[columns[1]].values() if x==-1]
     return data
 
 def split_train_test(data):
@@ -77,16 +69,15 @@ def validation(metrics, metrics_validation):
     return
 
 if __name__=="__main__":
-    path=r"/Users/tejaschaudhari/Desktop/hackathon/data.pkl"
+    path=None
     data=load_data(path)
-    print(data)
-    # preprocessed_data=preprocess_data(data)
+    preprocessed_data=preprocess_data(data)
 
-    # transformed_data = timeseries_transform(preprocessed_data)
-    # train,test,val=split_train_test(transformed_data)
-    # model_self=model_training(train,test)
-    # metrics=metric(val,model_self)
+    transformed_data = timeseries_transform(preprocessed_data)
+    train,test,val=split_train_test(transformed_data)
+    model_self=model_training(train,test)
+    metrics=metric(val,model_self)
     
-    # lstm_cm,lstm_balanced_accuracy=lstm(preprocessed_data,target='labels')
-    # metrics_validation = [lstm_cm, lstm_balanced_accuracy]
-    # validation(metrics,metrics_validation)
+    lstm_cm,lstm_balanced_accuracy=lstm(preprocessed_data,target='labels')
+    metrics_validation = [lstm_cm, lstm_balanced_accuracy]
+    validation(metrics,metrics_validation)
